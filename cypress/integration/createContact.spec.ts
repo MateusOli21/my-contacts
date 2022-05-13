c1f335ecc79d71d69cb2d4ecbaf491a4372a2163
MateusOli21 <mateus.oli.abreu@hotmail.com>
2022-05-13 16:15:53 -0300

    feat: add cypress and create first test

---
+++ cypress/integration/createContact.spec.ts
@@ -0,0 +1,29 @@
+describe('New contact', () => {
+  it('should create a new contact', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.findByRole('button', {
+      name: /novo contato/i,
+    }).click();
+
+    cy.findByRole('textbox', {
+      name: /Nome:/i,
+    }).type('Mateus');
+
+    cy.findByRole('textbox', {
+      name: /E-mail:/i,
+    }).type('mateus@gmail.com');
+
+    cy.findByRole('textbox', {
+      name: /Telefone:/i,
+    }).type('11999999999');
+
+    cy.findByRole('combobox', { name: /Categoria:/i }).select('instagram');
+
+    cy.findByRole('button', {
+      name: /Cadastrar/i,
+    }).click();
+
+    cy.findByText(/mateus@gmail.com/i).should('be.visible');
+  });
+});
